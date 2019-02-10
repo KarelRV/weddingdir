bae68081cd2325c98de5a173b1eb2196b6eef86c
Karel Verhoeven <karelverhoeven@Karels-MacBook-Pro.local>
2019-02-10 14:26:25 +0200

    some image logic

--- wapp/venues/urls.py
+++ wapp/venues/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     url(r'^venue_list', views.view_venues, name='venue_list'),
     url(r'^venue_info', views.venue_more_info, name='venue_info'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
